6e74d32a55036c8898e3a555e43b0d6ba09b35ea
Mikiyas6 <Mikitedi9@gmail.com>
2024-05-20 22:53:50 +0300

    Finishing my second assignment in data types

---
+++ 01-Fundamentals-Part-1/Assignments/assignment2/assignment2.js
@@ -0,0 +1,15 @@
+let country = "Ethiopia";
+let continent = "Africa";
+let population = 123400000;
+
+console.log("My country ->", country);
+console.log("My continent ->", continent);
+console.log("The population ->", population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,7 +1,1 @@
-console.log("My first JS code.");
 
-let firstName = "Jonas";
-
-console.log(firstName);
-
-console.log("noon");
