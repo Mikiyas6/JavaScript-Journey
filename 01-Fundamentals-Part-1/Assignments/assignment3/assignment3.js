0702a3f225b6e2e8ded55ddb1425c986fc89a489
Mikiyas6 <Mikitedi9@gmail.com>
2024-05-21 08:35:30 +0300

    The third assignment on "let, const, var"

---
+++ 01-Fundamentals-Part-1/Assignments/assignment3/assignment3.js
@@ -0,0 +1,23 @@
+const country = "Ethiopia";
+const continent = "Africa";
+let population = 123400000;
+
+console.log("My country ->", country);
+console.log("My continent ->", continent);
+console.log("The population ->", population);
+
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = "Amharic";
+
+console.log(language);
+
+country = "America"; // Assignment to a constant variable
+
+console.log(country);
