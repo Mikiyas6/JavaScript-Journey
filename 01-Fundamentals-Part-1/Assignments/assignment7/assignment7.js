b947cb009691fb6d1399d457161af8dc49987cc7
Mikiyas6 <Mikitedi9@gmail.com>
2024-05-23 21:30:01 +0300

    Assignment 7 was about type coersion

---
+++ 01-Fundamentals-Part-1/Assignments/assignment7/assignment7.js
@@ -0,0 +1,11 @@
+let a = "9" - "5"; // 4 -----> Number
+let b = "19" - "13" + "17"; // "617" -----> string
+let c = "19" - "13" + 17; //23 ----->  Number
+let d = "123" < 57; // False -----> Boolean
+let e = 5 + 6 + "4" + 9 - 4 - 2; // 1143 -----> Number
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,18 +1,40 @@
-const age = 15;
+// const age = 15;
 
-if (age >= 18) {
-  console.log(`Sarah can now start driving`);
-} else {
-  console.log(`She has ${18 - age} years left`);
-}
+// if (age >= 18) {
+//   console.log(`Sarah can now start driving`);
+// } else {
+//   console.log(`She has ${18 - age} years left`);
+// }
 
-const birthYear = 1999;
-let century;
+// const birthYear = 1999;
+// let century;
 
-if (birthYear <= 2000) {
-  century = 20;
-} else {
-  century = 21;
-}
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
 
-console.log(century);
+// console.log(century);
+
+// ################################
+// let a = "24";
+// let b = "23";
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+
+// let n = `1` + 1;
+// console.log(typeof n);
+// n -= 1;
+// console.log(typeof n);
+// console.log(n);
+// ########################################
+
+// ==    ->  does a type coercion
+// 18 == "18"  ->  True
+
+// ===   -> strict operator
+// 18 === 18   ->  True
+
+// Always use the Strict Operator
