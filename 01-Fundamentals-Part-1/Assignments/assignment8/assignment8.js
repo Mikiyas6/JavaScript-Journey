1551293bf21c974622bd67d52da1babe12f31c5c
Mikiyas6 <Mikitedi9@gmail.com>
2024-05-23 21:40:44 +0300

    Assignment 8 is about using type conversion while accepting an input from the user and then use strict operator when evaluating the input with another values.

---
+++ 01-Fundamentals-Part-1/Assignments/assignment8/assignment8.js
@@ -0,0 +1,13 @@
+numNeighbours = Number(
+  prompt("How many neighbour countries does your country have?")
+);
+
+if (numNeighbours === 1) {
+  console.log("Only 1 border");
+} else if (numNeighbours > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
+
+// Always use type conversion and strict operator
