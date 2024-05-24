e7e20f4fcc517362baf350574b7bba1878dff6fb
Mikiyas6 <Mikitedi9@gmail.com>
2024-05-24 11:47:33 +0300

    Assignment 11 is about using ternary operator instead of a simple If and else condition.

---
+++ 01-Fundamentals-Part-1/Assignments/assignment11/assignment11.js
@@ -0,0 +1,28 @@
+const country = "Ethiopia";
+const continent = "Africa";
+let population = 123400000;
+const isIsland = false;
+const language = "Amharic";
+
+console.log("My country ->", country);
+console.log("My continent ->", continent);
+console.log("The population ->", population);
+console.log("The language we speak ->", language);
+
+let halfPopulation = population / 2;
+
+const averagePopulation = 33000000;
+
+let isLessThanAverage = population < averagePopulation;
+
+description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
+// population > averagePopulation
+//   ? console.log(`${country}'s population is above average.`)
+//   : console.log(`${country}'s population is below average`);
+
+console.log(
+  `${country}'s populations is ${
+    population > averagePopulation ? "above" : "below"
+  } average`
+);
