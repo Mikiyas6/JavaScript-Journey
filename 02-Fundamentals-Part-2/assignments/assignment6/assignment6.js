2b7c48cef262c81c08e1d0e11d3953b1d8b36eb1
Mikiyas6 <Mikitedi9@gmail.com>
2024-05-26 13:13:50 +0300

    This assignment is about using objects to map related values.

---
+++ 02-Fundamentals-Part-2/assignments/assignment6/assignment6.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const myCountry = {
+  country: "Ethiopia",
+  capital: "Addis Ababa",
+  language: "Amharic",
+  population: 1234000000,
+  neighbors: ["Eritrea", "Sudan", "Somali"],
+};
