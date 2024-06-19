1d902334df71d8b6702c35e7b8b02c6459b95284
Mikiyas6 <Mikitedi9@gmail.com>
2024-06-19 13:13:52 +0300

    This challenge was about using Object Destructuring, Rest and Spread Operators and using Logical OR and Logical And Operators.

--- 09-Data-Structures-Operators/Challenges/challenge1/challenge1.js
+++ 09-Data-Structures-Operators/Challenges/challenge1/challenge1.js
@@ -39,3 +39,43 @@ const game = {
     team2: 6.5,
   },
 };
+
+// Task1
+const [players1, players2] = game.players;
+
+// Task2
+const [gk, ...fieldPlayers] = players1;
+
+// Task3
+const allPlayers = [...players1, ...players2];
+
+// Task4
+const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+// Task5
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1);
+
+// Task6
+let counter;
+const printGoals = function (...players) {
+  for (let i = 0; i < players.length; i++) {
+    counter = 0;
+    for (let j = 0; j < game.scored.length; j++) {
+      if (game.scored[j] === players[i]) {
+        counter += 1;
+      }
+    }
+    console.log(
+      `${players[i]} scored ${counter} goal${counter > 1 ? `s` : ''}`
+    );
+  }
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+// Task7
+console.log(
+  `${(team1 < team2 && game.team1) || game.team2} is more likely to win`
+);
