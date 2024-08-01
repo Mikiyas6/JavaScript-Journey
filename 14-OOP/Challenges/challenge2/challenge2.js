614b717abd0133a6450b7df9f261ebad368dd794
Mikiyas6 <Mikitedi9@gmail.com>
2024-08-01 21:41:28 +0300

    This challenge was about implementing the previous challenge using ES6 Classes and also implementing setters and getters

---
+++ 14-OOP/Challenges/challenge2/challenge2.js
@@ -0,0 +1,27 @@
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+}
+
+const Ford = new Car('Ford', 120);
+
+console.log(Ford.speedUS);
+Ford.accelerate();
+Ford.speedUS = 50;
+console.log(Ford.speed);
