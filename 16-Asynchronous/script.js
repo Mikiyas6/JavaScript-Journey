956a694f45d1cecafc0e294cdf4d4502d62a41b6
Mikiyas6 <Mikitedi9@gmail.com>
2024-08-19 13:14:54 +0300

    I learned about handling errors manually by using the catch method.

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -6,7 +6,6 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 const renderCountry = function (data) {
-  console.log(data);
   let html = `<article class="country">
         <img class="country__img" src="${data.flags.png}" />
         <div class="country__data">
@@ -27,6 +26,10 @@ const renderCountry = function (data) {
   countriesContainer.style.opacity = 1;
 };
 
+const renderError = function (msg) {
+  countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentText('beforeend', msg);
+};
 // const getCountryData = function (country) {
 //   // Ajax Call Country 1
 //   const request = new XMLHttpRequest();
@@ -68,16 +71,50 @@ const renderCountry = function (data) {
 // const request = fetch(`https://restcountries.com/v3.1/name/Ethiopia`);
 // console.log(request);
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(function (response) {
+    if (!response.ok) {
+      throw new Error(`${errorMsg} (${response.status})`);
+    }
+    return response.json();
+  });
+};
+
 const getCountryData = function (country) {
   // fetch will return a promise and we can call the then method.
   // In the then method, we need to pass a callback function that will be executed when the promise is fullfilled
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(function (response) {
-      return response.json();
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country Not Found')
+    .then(function ([data]) {
+      renderCountry(data);
+      const neighbors = data.borders;
+      if (!neighbors) {
+        throw new Error('No Neighbors Found');
+      }
+      neighbors.forEach(function (neighbor) {
+        return getJSON(
+          `https://restcountries.com/v3.1/alpha/${neighbor}`,
+          'Neighbor Not FOund'
+        )
+          .then(function (data) {
+            renderCountry(data[0]);
+          })
+          .catch(function (err) {
+            renderError(
+              `💥💥💥 Something Went Wrong(${err.message}). Try again!`
+            );
+          })
+          .finally(function () {
+            countriesContainer.style.opacity = 1;
+          });
+      });
     })
-    .then(function (data) {
-      renderCountry(data[0]);
+    .catch(err => {
+      renderError(`💥💥💥 Something Went Wrong(${err.message}). Try again!`);
+    })
+    .finally(function () {
+      countriesContainer.style.opacity = 1;
     });
 };
-getCountryData('Ethiopia');
-getCountryData('Morocco ');
+btn.addEventListener('click', function () {
+  getCountryData('Australia');
+});
